7adc59a58500257b85ec1270c13d94d62f53c634
Letícia Borchardt <leticiaborchardt123@gmail.com>
2023-11-24 09:03:51 -0300

    Aula 24/11

---
+++ Modulo5/WebIsaakNewton/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebIsaakNewton.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
